324a01155517b484d775e467fc0a035857ea1547
a18sullivan <a18sullivan@student.bridgew.edu>
2020-01-23 11:35:03 -0500

    added a comment to add my name

--- main.java
+++ main.java
@@ -1,9 +1,8 @@
+// Alexander Sullivan
 public class main {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("hi");
-
-
     }
 
 }
