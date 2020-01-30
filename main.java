ec1903af8938ee03e1616ca6ac1c3cebb02fa8e3
a18sullivan <a18sullivan@student.bridgew.edu>
2020-01-30 11:21:38 -0500

    file io name picker

--- main.java
+++ main.java
@@ -1,36 +1,22 @@
-// Alexander Sullivan
-//scanner stuff
-/*
-import java.util.Scanner;
 
-public class main {
-    public static void main(String[] args){
-        var reader= new Scanner(System.in);
-        System.out.println("what is your name");
-        String name=reader.nextLine();
-        System.out.println("Hello "+name);
-        System.out.println("hi");
-    }
-
-}
-*/
-//File IO stuff
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Random;
 
-public class FileExample{
-    public static void main(String[] args){
+
+public class main{
+    public static void main(String[] args)throws IOException{
         var fileName = "students.txt";
-        String all_names = Files.readString(Paths.get(fileName);
-        String[] name_list
+        String all_names = Files.readString(Paths.get(fileName));
+        String[] name_list = all_names.split( "\n");
+        var numberOfStudents = name_list.length;
+        var randGenerator = new Random();
+        var num=randGenerator.nextInt(numberOfStudents);
+        System.out.println("Congrats "+ name_list[num]+ " you get to answer the question ");
 
 
     }
 
 }
-//.equals is used to compare strings
-//import java.util.Random;
-//import java.util.Scanner;
-//var randGenerator = new Random();
 
